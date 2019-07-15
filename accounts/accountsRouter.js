777fac7ca6de92956a8981fbe3e16bfae29e55f0
Jeff Oliver <jeffoliver3384@gmail.com>
2019-07-15 17:10:39 -0400

    created endpoints to get account by id and update account by id

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -11,10 +11,54 @@ const db = knex({
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
   db('accounts')
     .then(accounts => {
       res.status(200).json(accounts);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  db('accounts')
+    .where({ id: id })
+    .first()
+    .then(account => {
+      res.status(200).json(account);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post('/', (req, res) => {
+  const account = req.body;
+
+  db('accounts')
+    .insert(account, 'id')
+    .then(arrayOfIds => {
+      const idOfLastRecord = arrayOfIds[0];
+
+      res.status(201).json(idOfLastRecord);
+    })
+    .catch(error => res.status(500).json(error));
+});
+
+router.put('/:id', (req, res) => {
+  const {
+    params: { id },
+    body: body,
+  } = req;
+  db('accounts')
+    .where({ id: id })
+    .update(body)
+    .then(account => {
+      res.status(200).json({ message: `${account} record(s) updated` });
     })
     .catch(error => {
       res.status(500).json(error);
